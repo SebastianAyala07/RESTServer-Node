dac696fb7f4ed996195a5ee5525fb5dc93fd5131
Sebastian Ayala Gonzalez <sebaxayala@gmail.com>
2021-07-24 16:34:39 -0500

    [ADD] Se inicia el proyecto - Instalando sus dependencias principales (express y dotenv)

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+app.get('/', ( req, res ) => {
+	res.send('Hello World')
+});
+
+app.listen( process.env.PORT , () => {console.log('Servidor corriendo en', process.env.PORT)});
+
